9351b3b761b1357f00c315e23a3dbe96e429deca
Killian <killian.delamarre-ruer@students.dominican.edu>
2026-01-12 13:40:56 -0800

    basic flask app beginning

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+import serial
+import json
+import threading
+import time
 
 app = Flask(__name__)
-
+CORS(app)
 
 
 latest_reading = {
@@ -12,3 +17,8 @@ latest_reading = {
 }
 
 SERIAL_PORT = '/dev/cu.usbserial-XXXX'
+BAUD_RATE = 115200
+
+def read_serial_continuously():
+    """placeholder"""
+    global latest_reading
